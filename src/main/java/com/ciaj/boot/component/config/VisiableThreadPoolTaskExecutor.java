399aa848a1daac50ddee0e572d770b1647590618
revolver <595009116@qq.com>
2019-08-24 15:58:47 +0800

    ..

---
+++ src/main/java/com/ciaj/boot/component/config/VisiableThreadPoolTaskExecutor.java
@@ -0,0 +1,69 @@
+package com.ciaj.boot.component.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @Description TODO
+ * @Author Ciaj.
+ * @Date 2019/8/14 11:14
+ * @Version 1.0
+ */
+@Log4j2
+public class VisiableThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
+	private void showThreadPoolInfo(String prefix) {
+		ThreadPoolExecutor threadPoolExecutor = getThreadPoolExecutor();
+		if (null == threadPoolExecutor) {
+			return;
+		}
+		log.info("{},{},taskCount[{}],completedTaskCount[{}],activeCount[{}],queuesize[{}]",
+				this.getThreadNamePrefix(),
+				prefix,
+				threadPoolExecutor.getTaskCount(),
+				threadPoolExecutor.getCompletedTaskCount(),
+				threadPoolExecutor.getActiveCount(),
+				threadPoolExecutor.getQueue().size()
+		);
+	}
+
+	@Override
+	public void execute(Runnable task) {
+		showThreadPoolInfo("1. do execute");
+		super.execute(task);
+	}
+
+	@Override
+	public void execute(Runnable task, long startTimeout) {
+		showThreadPoolInfo("2. do execute");
+		super.execute(task, startTimeout);
+	}
+
+	@Override
+	public Future<?> submit(Runnable task) {
+		showThreadPoolInfo("1. do submit");
+		return super.submit(task);
+	}
+
+	@Override
+	public <T> Future<T> submit(Callable<T> task) {
+		showThreadPoolInfo("2. do submit");
+		return super.submit(task);
+	}
+
+	@Override
+	public ListenableFuture<?> submitListenable(Runnable task) {
+		showThreadPoolInfo("1. do submitListenable");
+		return super.submitListenable(task);
+	}
+
+	@Override
+	public <T> ListenableFuture<T> submitListenable(Callable<T> task) {
+		showThreadPoolInfo("2. do submitListenable");
+		return super.submitListenable(task);
+	}
+}
