2915568879724a1c32a9c3379b318b1024a7e709
ciaj <595009116@qq.com>
2025-08-22 12:31:12 +0800

    表单验证

--- src/main/java/com/ciaj/comm/utils/RequestUtils.java
+++ src/main/java/com/ciaj/comm/utils/RequestUtils.java
@@ -63,36 +63,7 @@ public class RequestUtils {
 	 */
 	public static String getRemoteAddr() {
 		HttpServletRequest request = getRequest();
-		String ip = null;
-		try {
-			ip = request.getHeader("x-forwarded-for");
-			if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getHeader("Proxy-Client-IP");
-			}
-			if (StringUtils.isEmpty(ip) || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getHeader("WL-Proxy-Client-IP");
-			}
-			if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getHeader("HTTP_CLIENT_IP");
-			}
-			if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-			}
-			if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getRemoteAddr();
-			}
-		} catch (Exception e) {
-			log.error("IPUtils ERROR ", e);
-		}
-
-		//使用代理，则获取第一个IP地址
-		if (StringUtils.isEmpty(ip) && ip.length() > 15) {
-			if (ip.indexOf(",") > 0) {
-				ip = ip.substring(0, ip.indexOf(","));
-			}
-		}
-
-		return ip;
+		return getRemoteAddr(request);
 	}
 
 	/**
